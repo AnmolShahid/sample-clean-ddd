189dbb7f9717007875b71082a1f1a7fa64395897
anmolshahhid@hotmail.com <anmolshahid@hotmail.com>
2022-09-02 12:10:37 +1000

    commit

---
+++ lib/common/exceptions/server_exceptions.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+class ServerException implements Equatable {
+  @override
+  List<Object> get props => null;
+
+  call() {
+    return null;
+  }
+
+  @override
+  bool get stringify => null;
+}
